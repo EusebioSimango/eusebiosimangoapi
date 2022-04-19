06bc1be338dde26d1cdcb57aeaae30a881fac703
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-04-19 23:58:01 +0200

    setting cors

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ const handler = (request, response) => {
 	response.writeHead(200, DEFAULT_HEADER)
 
 	const key = `/${api}:${method.toLowerCase()}`
-
+	
+	response.setHeader('Access-Control-Allow-Origin', '*');
+	response.setHeader('Access-Control-Request-Method', '*');
+	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	response.setHeader('Access-Control-Allow-Headers', '*');
 	response.writeHead(200, DEFAULT_HEADER)
 
 	const chosen = routes[key] || routes.default
